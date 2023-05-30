33cda139c6f1642d17a42ebd8ad0eb2c3027433b
staturnz <118011249+staturnzz@users.noreply.github.com>
2023-05-30 00:35:07 -0500

    feat: arch detection, fix current_time

--- install.sh
+++ install.sh
@@ -22,14 +22,39 @@ NO_COLOR='\033[0m'
 # =========
 # Variables
 # =========
+alias current_time="date +'%m/%d/%y %H:%M:%S'"
+
 os=$(uname)
 
 case "$os" in
     Linux)
-        arch=$(arch)
+        arch_check=$(arch)
     ;;
     Darwin)
-        arch=$(uname -m)
+        arch_check=$(uname -m)
+    ;;
+    *)
+        echo " - [${YELLOW}$(current_time)${NO_COLOR}] ${RED}<Error>${NO_COLOR}: ${RED}Unknown or unsupported OS.${NO_COLOR}"
+        exit 1
+    ;;
+esac
+
+case "$arch_check" in
+    x86_64* | amd64)
+        arch=x86_64
+    ;;
+    i?86 | x86*)
+        arch=x86
+    ;;
+    aarch64* | arm64*)
+        arch=arm64
+    ;;
+    arm*)
+        arch=armel
+    ;;
+    *)
+        echo " - [${YELLOW}$(current_time)${NO_COLOR}] ${RED}<Error>${NO_COLOR}: ${RED}Unknown or unsupported architecture.${NO_COLOR}"
+        exit 1
     ;;
 esac
 
@@ -39,13 +64,12 @@ esac
 
 [ "$os" = "Linux" ] && {
     [[ $(grep -i Microsoft /proc/version) ]] && {
-        echo " - [${YELLOW}$current_time${NO_COLOR}] ${RED}<Error>${NO_COLOR}: ${RED}WSL is not supported in this install script.${NO_COLOR}"
+        echo " - [${YELLOW}$(current_time)${NO_COLOR}] ${RED}<Error>${NO_COLOR}: ${RED}WSL is not supported in this install script.${NO_COLOR}"
     }
 }
 
 [ "$(id -u)" -ne 0 ] && {
-    current_time=$(date +"%m/%d/%y %H:%M:%S")
-    echo " - [${YELLOW}$current_time${NO_COLOR}] ${YELLOW}<Warning>${NO_COLOR}: ${YELLOW}In order to use this script, run with root or use sudo.${NO_COLOR}"
+    echo " - [${YELLOW}$(current_time)${NO_COLOR}] ${YELLOW}<Warning>${NO_COLOR}: ${YELLOW}In order to use this script, run with root or use sudo.${NO_COLOR}"
     exit 1
 }
 
